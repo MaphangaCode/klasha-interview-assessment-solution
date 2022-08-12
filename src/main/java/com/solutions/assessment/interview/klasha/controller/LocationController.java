d3f9e6ac31c56bb4079f2021c5a155fa16d3597b
MaphangaCode <Maphanga.ng@gmail.com>
2022-08-12 03:02:10 +0200

    Added controller for location

--- src/main/java/com/solutions/assessment/interview/klasha/controller/LocationController.java
+++ src/main/java/com/solutions/assessment/interview/klasha/controller/LocationController.java
@@ -1,20 +1,33 @@
 package com.solutions.assessment.interview.klasha.controller;
 
 import com.solutions.assessment.interview.klasha.domain.dto.GenericPagedResponseDto;
+import com.solutions.assessment.interview.klasha.domain.dto.LocationWarehouseDto;
+import com.solutions.assessment.interview.klasha.domain.dto.ShortestPathDto;
 import com.solutions.assessment.interview.klasha.domain.dto.location.*;
 import com.solutions.assessment.interview.klasha.service.LocationService;
+import com.solutions.assessment.interview.klasha.service.LocationWarehouseService;
+import com.solutions.assessment.interview.klasha.service.ShortPathFinderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/location/")
 public class LocationController {
 
     private final LocationService locationService;
+    private final ShortPathFinderService shortPathFinderService;
 
-    public LocationController(final LocationService locationService) {
+    private final LocationWarehouseService locationWarehouseService;
+
+    public LocationController(final LocationService locationService,
+                              final ShortPathFinderService shortPathFinderService,
+                              final LocationWarehouseService locationWarehouseService) {
         this.locationService = locationService;
+        this.shortPathFinderService = shortPathFinderService;
+        this.locationWarehouseService = locationWarehouseService;
     }
 
     @PostMapping("add")
@@ -49,11 +62,34 @@ public class LocationController {
         return new ResponseEntity<>(locationDto, HttpStatus.OK);
     }
 
-    @GetMapping("optimal-route/{originLocationId}/{destinationLocationId}")
-    public ResponseEntity<OptimalRouteDto> getOptimalRoute(@PathVariable final Long originLocationId,
-                                                           @PathVariable final Long destinationLocationId) {
-        final OptimalRouteDto optimalRouteDto = new OptimalRouteDto();
-        return new ResponseEntity<>(optimalRouteDto, HttpStatus.OK);
+    @GetMapping("shortest-route/{originLocationId}/{destinationLocationId}")
+    public ResponseEntity<ShortestPathDto> getShortestRoute(@PathVariable final Long originLocationId,
+                                                            @PathVariable final Long destinationLocationId) {
+        final ShortestPathDto shortestPathDto = shortPathFinderService.getShortestPath(originLocationId, destinationLocationId);
+
+        return new ResponseEntity<>(shortestPathDto, HttpStatus.OK);
+    }
+
+    @PostMapping("connect/{originLocationId}")
+    public ResponseEntity<LocationDto> connectLocation(@PathVariable final Long originLocationId,
+                                                       @RequestBody final List<Long> destinations) {
+        final LocationDto locationDto = locationService.connectLocation(originLocationId, destinations);
+
+        return new ResponseEntity<>(locationDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("remove/{locationId}")
+    public void removeLocation(@PathVariable final Long locationId) {
+        locationService.removeLocation(locationId);
+    }
+
+
+    @PostMapping("add-packages/{locationId}/{packageCount}")
+    public ResponseEntity<LocationWarehouseDto> registerPackages(@PathVariable final Long locationId,
+                                                                 @PathVariable final Integer packageCount) {
+        final LocationWarehouseDto locationWarehouseDto = locationWarehouseService.addPackages(locationId, packageCount);
+
+        return new ResponseEntity<>(locationWarehouseDto, HttpStatus.OK);
     }
 
 }
