07ccb2efdb6b624b1f289056af8c04e7283395d3
MaphangaCode <Maphanga.ng@gmail.com>
2022-08-12 03:00:26 +0200

    Added exceptions

--- src/main/java/com/solutions/assessment/interview/klasha/domain/exception/ResourceInvalidStateException.java
+++ src/main/java/com/solutions/assessment/interview/klasha/domain/exception/ResourceInvalidStateException.java
@@ -1,2 +1,8 @@
-package com.solutions.assessment.interview.klasha.domain.exception;public class ResourceInvalidStateException {
+package com.solutions.assessment.interview.klasha.domain.exception;
+
+public class ResourceInvalidStateException extends RuntimeException {
+
+    public ResourceInvalidStateException(final String message) {
+        super(message);
+    }
 }
